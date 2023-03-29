0606c1f0ed0e7791377f1e8c245f8abac63cdf5e
Ryan Silvera <ryan.silvera@icloud.com>
2023-03-28 23:19:23 -0400

    First test attempt

---
+++ js/calculator.js
@@ -0,0 +1,22 @@
+function calculator (a, operator, b) {
+    if (operator === '+'){
+        let add = a + b
+        return add
+    }
+    if (operator === '-'){
+        let subtract = a - b
+        return subtract
+    }
+    if (operator === 'x'){
+        let multiply = a * b
+        return multiply
+    }
+    if (operator === '/'){
+        let divide = a / b
+        return divide
+    }
+
+    return
+}
+
+module.exports = calculator;
